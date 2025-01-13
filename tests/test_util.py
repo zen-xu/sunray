885ae15ba49e3e420876c53f8cb7113c1f1d4b57
ZhengYu, Xu <zen-xu@outlook.com>
2025-01-13 16:00:17 +0800

    feat: impl ActorPool

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,7 +1,10 @@
 # ruff: noqa: I002
 from typing import Tuple  # noqa: UP035
 
+import sunray
+
 from sunray._internal.util import get_num_returns
+from sunray.util import ActorPool
 
 
 def func1() -> Tuple[int, float]:  # noqa: UP006
@@ -25,3 +28,23 @@ def test_get_num_returns():
     assert get_num_returns(func2) == 2
     assert get_num_returns(func3) == 1
     assert get_num_returns(func4) == 1
+
+
+def test_actor_pool(init_ray):
+    class Actor(sunray.ActorMixin):
+        @sunray.remote_method
+        def double(self, v: int) -> int:
+            return 2 * v
+
+    a1, a2 = Actor.new_actor().remote(), Actor.new_actor().remote()
+    pool = ActorPool([a1, a2])
+    assert list(pool.map(lambda a, v: a.methods.double.remote(v), [1, 2, 3, 4])) == [
+        2,
+        4,
+        6,
+        8,
+    ]
+
+    assert set(
+        pool.map_unordered(lambda a, v: a.methods.double.remote(v), [1, 2, 3, 4])
+    ) == {2, 4, 6, 8}
